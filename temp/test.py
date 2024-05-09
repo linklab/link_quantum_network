ac4166c6559dfc7fe7b4bd5a8e55f522fed978c3
YHHNA_mac_mini <yh21.han@gmail.com>
2024-05-09 17:16:33 +0900

    ...

--- temp/test.py
+++ temp/test.py
@@ -1,2 +1,37 @@
 import netsquid as ns
-ns.test()
+print(ns.__version__)
+qubits = ns.qubits.create_qubits(num_qubits=1)
+
+qubit = qubits[0]
+
+print("1." + "-" * 50)
+print(ns.qubits.reduced_dm(qubit))
+print("1." + "-" * 50)
+
+ns.qubits.operate(qubits=qubit, operator=ns.X)
+
+print("2." + "-" * 50)
+print(ns.qubits.reduced_dm(qubit))
+print("2." + "-" * 50)
+
+measurement_result, prob = ns.qubits.measure(qubit=qubit, observable=ns.Z)
+if measurement_result == 0:
+    state = "|0>"
+else:
+    state = "|1>"
+
+print("3." + "-" * 50)
+print(f"Measured {state} with probability {prob:.1f}")
+print(ns.qubits.reduced_dm(qubit))
+print("3." + "-" * 50)
+
+measurement_result, prob = ns.qubits.measure(qubit=qubit, observable=ns.X)
+if measurement_result == 0:
+    state = "|+>"
+else:
+    state = "|->"
+
+print("4." + "-" * 50)
+print(f"Measured {state} with probability {prob:.1f}")
+print(ns.qubits.reduced_dm(qubit))
+print("4." + "-" * 50)
